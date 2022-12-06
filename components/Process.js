b85ae60e6dc8e2093def02d3b416f63f8dcc4cc0
Justine <ujusine@yahoo.com>
2022-12-06 13:36:08 +0800

    Add class to span

--- components/Process.js
+++ components/Process.js
@@ -8,9 +8,10 @@ const Proccess = () => {
           <span className={styles.spanintro}>How we work</span>
           <h2 className={styles.h2}>The 3 steps of our development process</h2>
           <p className={styles.p}>
-            Iskulbukul follow 3 major steps for the development of our websites and applications. 
-            The projects consist of Planning, Designing, and Development processes to fulfill 
-            and accomplish the requests of our clients.
+            Iskulbukul follow 3 major steps for the development of our websites
+            and applications. The projects consist of Planning, Designing, and
+            Development processes to fulfill and accomplish the requests of our
+            clients.
           </p>
           <img className={styles.img} src="/process.svg" alt="" />
         </div>
@@ -18,35 +19,39 @@ const Proccess = () => {
           <div>
             <span className={styles.number}>1</span>
             <div>
-              <span>Planning</span>
+              <span className={styles.processspan}>Planning</span>
               <p className={styles.p}>
-                Iskulbukul is always establishing goals and objectives for a specific time frame, 
-                coming up with potential courses of action, and then selecting the best course of 
-                action among the available options.
+                Iskulbukul is always establishing goals and objectives for a
+                specific time frame, coming up with potential courses of action,
+                and then selecting the best course of action among the available
+                options.
               </p>
             </div>
           </div>
           <div>
             <span className={styles.number}>2</span>
             <div>
-              <span>Designing</span>
+              <span className={styles.processspan}>Designing</span>
               <p className={styles.p}>
-                Iskulbukul always use different color's, layout, font, and images, mostly from scratch.
-                We use Photoshop, Illustrator, Fireworks, and Sketch for implementing designs and visuals
-                for the applications. We are 100% sure that are designs are unique and engaging for our
-                clients.
+                Iskulbukul always use different color's, layout, font, and
+                images, mostly from scratch. We use Photoshop, Illustrator,
+                Fireworks, and Sketch for implementing designs and visuals for
+                the applications. We are 100% sure that are designs are unique
+                and engaging for our clients.
               </p>
             </div>
           </div>
           <div>
             <span className={styles.number}>3</span>
             <div>
-              <span>Development</span>
+              <span className={styles.processspan}>Development</span>
               <p className={styles.p}>
-                Iskulbukul always plan and decide ahead of time for the development of their projects.
-                Here at Iskulbukul, we always work as a team to finish tasks commissioned by our clients.
-                Our projects can range from a simple static page of plain text to complex web applications, 
-                electronic businesses, and social network services.
+                Iskulbukul always plan and decide ahead of time for the
+                development of their projects. Here at Iskulbukul, we always
+                work as a team to finish tasks commissioned by our clients. Our
+                projects can range from a simple static page of plain text to
+                complex web applications, electronic businesses, and social
+                network services.
               </p>
             </div>
           </div>
